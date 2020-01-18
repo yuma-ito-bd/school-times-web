38ec5d563da1af1d060a401a46c02fc19461d006
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2020-01-18 21:26:14 +0900

    プロジェクト名を変更

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to school-times!');
+    expect(page.getTitleText()).toEqual('Welcome to school-times-web!');
   });
 
   afterEach(async () => {
