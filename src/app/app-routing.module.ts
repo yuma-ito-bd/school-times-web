b1d157f467642ae04102f6a9329f3369c0c5c733
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2020-09-21 14:42:12 +0900

    ログインコンポーネントを作成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './authentication/login/login.component';
 import { TopComponent } from './top/top.component';
 import { ViewArticleComponent } from './view-article/view-article.component';
 
@@ -7,6 +8,7 @@ const routes: Routes = [
     { path: '', component: TopComponent, pathMatch: 'full' },
     { path: 'view/:id', component: ViewArticleComponent },
     { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+    { path: 'login', component: LoginComponent },
     { path: '**', redirectTo: '' },
 ];
 
--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,11 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { LoginComponent } from './login/login.component';
-
 
 @NgModule({
     declarations: [LoginComponent],
-    imports: [CommonModule, AuthenticationRoutingModule],
+    imports: [CommonModule],
 })
 export class AuthenticationModule {}
