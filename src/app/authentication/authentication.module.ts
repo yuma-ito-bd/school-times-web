01ef285f8c3d1fffa15df5982825cf37a3dc8486
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2020-09-21 18:36:42 +0900

    ログイン画面にfirebaseUIを設置

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FirebaseUIModule } from 'firebaseui-angular';
 import { LoginComponent } from './login/login.component';
 
 const components = [LoginComponent];
 
 @NgModule({
     declarations: [...components],
-    imports: [CommonModule],
+    imports: [CommonModule, FirebaseUIModule],
     exports: [...components],
 })
 export class AuthenticationModule {}
