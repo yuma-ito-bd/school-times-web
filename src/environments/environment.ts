98f5070fd3a492ca1fd11d5c17fa9e56522b430d
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2020-09-21 12:46:40 +0900

    環境変数の設定

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,17 @@
 export const environment = {
     production: false,
     apiEndpoint: 'http://localhost:3000',
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    firebaseConfig: {
+        apiKey: 'AIzaSyAm4MG_ZoFy11kFoR1FotPKJ-FJWDlfYaw',
+        authDomain: 'schooltimes-2227d.firebaseapp.com',
+        databaseURL: 'https://schooltimes-2227d.firebaseio.com',
+        projectId: 'schooltimes-2227d',
+        storageBucket: 'schooltimes-2227d.appspot.com',
+        messagingSenderId: '358503067376',
+        appId: '1:358503067376:web:ca7bc6fb704b7ba21a5c35',
+        measurementId: 'G-2QQ7255HY3',
+    },
 };
 
 /*
