8b9b2924fe532248b13ea3660c75096ae9b99256
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2020-09-22 17:46:26 +0900

    TOP画面のpathにAuthガードを適用

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './authentication/login/login.component';
+import { AuthGuard } from './guards/auth/auth.guard';
 import { TopComponent } from './top/top.component';
 import { ViewArticleComponent } from './view-article/view-article.component';
 
 const routes: Routes = [
-    { path: 'top', component: TopComponent },
+    { path: 'top', component: TopComponent, canActivate: [AuthGuard] },
     { path: 'view/:id', component: ViewArticleComponent },
     { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
     { path: 'login', component: LoginComponent },
-    { path: '**', redirectTo: 'top' },
+    { path: '**', redirectTo: 'login' },
 ];
 
 @NgModule({
