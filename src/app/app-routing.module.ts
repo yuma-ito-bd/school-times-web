29ff5d83da9b5590c2800094d90409eb8fbd77ad
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2020-09-22 18:14:29 +0900

    管理画面のpathにガードを適応

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './authentication/login/login.component';
+import { AdminGuard } from './guards/admin/admin.guard';
 import { AuthGuard } from './guards/auth/auth.guard';
 import { TopComponent } from './top/top.component';
 import { ViewArticleComponent } from './view-article/view-article.component';
 
 const routes: Routes = [
     { path: 'top', component: TopComponent, canActivate: [AuthGuard] },
-    { path: 'view/:id', component: ViewArticleComponent },
-    { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+    { path: 'view/:id', component: ViewArticleComponent, canActivate: [AuthGuard] },
+    {
+        path: 'admin',
+        loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
+        canActivate: [AdminGuard],
+    },
     { path: 'login', component: LoginComponent },
     { path: '**', redirectTo: 'login' },
 ];
